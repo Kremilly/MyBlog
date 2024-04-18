541ba0d1a089bbd1b3abfeae2876458bd941667f
kremilly <contact@kremilly.com>
2024-04-18 14:50:11 -0300

    feat: add initial code base

--- index.py
+++ index.py
@@ -1,15 +1,19 @@
-from flask import Flask
+import markdown2
+
+from markupsafe import Markup
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 @app.route("/blog")
 @app.route("/blog/<post>")
 def blog(post=None):
-    return f"<p>Hello, {post}!</p>"
+    html_content = markdown2.markdown(f'# Hello {post}')
+    return render_template('blog.html', html_content=Markup(html_content))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
