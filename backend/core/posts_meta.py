166673e2707f53b6b193bf6ac28b2cfd594d1823
kremilly <contact@kremilly.com>
2024-04-19 18:18:56 -0300

    rewrite: rewrite core/posts_meta.py

--- backend/core/posts_meta.py
+++ backend/core/posts_meta.py
@@ -2,22 +2,31 @@
 
 import re, markdown2
 
+from bs4 import BeautifulSoup
+
 from backend.utils.files import FilesUtils
 
 from backend.classes.settings import Settings
 
 class PostsMeta:
-
+    
     @classmethod
-    def post_title(cls, file:str) -> str|None:
+    def get_post_content(cls, file:str) -> str|None:
         file = file.lower().replace('-', ' ') + '.md'
         file_path = Settings.get('paths.contents', 'string') + file
 
         markdown_content = FilesUtils.read_content(file_path)
         
         if markdown_content is not None:
-            html_content = markdown2.markdown(markdown_content)
+            return markdown2.markdown(markdown_content)
+        
+        return None
 
+    @classmethod
+    def post_title(cls, file:str) -> str|None:
+        html_content = cls.get_post_content(file)
+        
+        if html_content is not None:
             index_h1 = html_content.find('<h1>')
             
             if index_h1 != -1:
@@ -33,14 +42,9 @@ class PostsMeta:
     
     @classmethod
     def post_cover(cls, file:str) -> str|None:
-        file = file.lower().replace('-', ' ') + '.md'
-        file_path = Settings.get('paths.contents', 'string') + file
-
-        markdown_content = FilesUtils.read_content(file_path)
+        html_content = cls.get_post_content(file)
         
-        if markdown_content is not None:
-            html_content = markdown2.markdown(markdown_content)
-
+        if html_content is not None:
             img_match = re.search(r'<img[^>]*src="([^"]+)"[^>]*>', html_content)
 
             if img_match:
@@ -51,6 +55,28 @@ class PostsMeta:
                 return None
             
         return None
+    
+    @classmethod
+    def post_topics(cls, file:str) -> dict:
+        html_content = cls.get_post_content(file)
+        
+        if html_content is not None:
+            headers = []
+            soup = BeautifulSoup(html_content, 'html.parser')
+            
+            for header in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+                anchor_id = re.sub(
+                    r'[^\w\s-]', '', header.text.lower().strip().replace(' ', '-')
+                )
+                
+                header['id'] = anchor_id
+                
+                headers.append({
+                    'id': anchor_id,
+                    'text': header.text, 
+                })
+            
+            return headers
 
     @classmethod
     def head_post_title(cls, file:str)-> str:
