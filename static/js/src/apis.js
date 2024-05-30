48326ff9583beef8e3d58cdd825beadd85a3ad33
Kremilly <contact@kremilly.com>
2024-05-29 21:07:46 -0300

    feat: making a minors visual changes

--- static/js/src/apis.js
+++ static/js/src/apis.js
@@ -7,7 +7,7 @@ const Apis = ( e => {
     let format = (number) => {
 		if (number >= 1000) {
 			const milhares = number / 1000
-			return milhares + "k"
+			return milhares + 'k'
 		} else {
 			return number.toString()
 		}
@@ -20,7 +20,7 @@ const Apis = ( e => {
         }).then(
             json => json.json()
         ).then(response => {
-            if (!no_anchor) window.location.hash = "pins"
+            if (!no_anchor) window.location.hash = 'pins'
 
             $('.featured-tabs').removeClass('actived')
             $('#tabPins').toggleClass('actived')
@@ -29,10 +29,13 @@ const Apis = ( e => {
 
             response.forEach(item => {
                 $('#featuredList').append(`
-                    <a href="${item.url}" target="_blank" class="item">
-                        <div class="name">${item.name}</div>
-                        <div class="info">${item.description}</div>
-                        <div class="footer">${item.languages[0]} • ${format(item.stars)} stars • ${format(item.forks)} forks</div>
+                    <a href='${item.url}' target='_blank' class='item'>
+                        <div class='name'>${item.name}</div>
+                        <div class='info'>${item.description}</div>
+                        
+                        <div class='footer'>
+                            ${item.languages[0]} • <div class='fas fa-star'></div>${format(item.stars)} stars • <div class='fas fa-code-fork'></div> ${format(item.forks)} forks
+                        </div>
                     </a>
                 `)
             })
@@ -45,7 +48,7 @@ const Apis = ( e => {
         fetch(`${apiUri}`).then(
             json => json.json()
         ).then(response => {
-            window.location.hash = "apis"
+            window.location.hash = 'apis'
 
             $('.featured-tabs').removeClass('actived')
             $('#tabApis').toggleClass('actived')
@@ -56,8 +59,8 @@ const Apis = ( e => {
                 let title = item.name.charAt(0).toUpperCase() + item.name.slice(1)
 
                 $('#featuredList').append(`
-                    <a href="${item.wiki}" target="_blank" class="item">
-                        <div class="name">${title}</div>
+                    <a href='${item.wiki}' target='_blank' class='item'>
+                        <div class='name'>${title}</div>
                     </a>
                 `)
             })
@@ -70,7 +73,7 @@ const Apis = ( e => {
         fetch(`https://crates.io/api/v1/crates?user_id=${user_id}`).then(
             json => json.json()
         ).then(response => {
-            window.location.hash = "crates"
+            window.location.hash = 'crates'
 
             $('.featured-tabs').removeClass('actived')
             $('#tabCrates').toggleClass('actived')
@@ -81,10 +84,10 @@ const Apis = ( e => {
                 let title = item.name.charAt(0).toUpperCase() + item.name.slice(1)
 
                 $('#featuredList').append(`
-                    <a href="https://crates.io/crates/${item.name}" target="_blank" class="item">
-                        <div class="name">${title}</div>
-                        <div class="info">${item.description}</div>
-                        <div class="footer">${format(item.downloads)} downloads • ${item.newest_version}</div>
+                    <a href='https://crates.io/crates/${item.name}' target='_blank' class='item'>
+                        <div class='name'>${title}</div>
+                        <div class='info'>${item.description}</div>
+                        <div class='footer'>${format(item.downloads)} downloads • ${item.newest_version}</div>
                     </a>
                 `)
             })
