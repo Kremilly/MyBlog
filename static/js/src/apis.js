7700851fdfddef5cbc1c0fbb6bd7ddb4b1d3a73b
Kremilly <contact@kremilly.com>
2024-06-02 04:29:38 -0300

    feat: update fetch endpoint in downloadPdf function

--- static/js/src/apis.js
+++ static/js/src/apis.js
@@ -155,7 +155,11 @@ const Apis = ( e => {
     }
 
     let downloadPdf = e => {
-        fetch(`${ window.location.href }/export`).then(response => {
+        let endpoint = window.location.href.replace(
+            window.location.hash, ''
+        ) + '/export'
+
+        fetch(`${ endpoint }`).then(response => {
             if (!response.ok) { throw new Error('Network response was not ok') }
             return response.url
         }).then( url => {
