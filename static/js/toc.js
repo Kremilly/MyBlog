1424da5ee03f7e5a087dec0c400a6a5540ebd8a4
Kremilly <contact@kremilly.com>
2024-07-03 06:55:56 -0300

    feat: new TOC menu implemented

---
+++ static/js/toc.js
@@ -0,0 +1,69 @@
+$( e => {
+
+    var tocList = $('#tocList');
+    var headers = $('h1, h2, h3');
+    var currentHash = window.location.hash;
+
+    $('#tocBoxBtn').click( function () {
+        $(this).toggleClass('actived');
+        $('#tocBox').slideToggle(250);
+    });
+
+    $('.search').on('input', function () {
+        var searchText = $(this).val().toLowerCase();
+
+        $('.list').each( function () {
+            var listItemText = $(this).text().toLowerCase();
+
+            if (listItemText.includes(searchText)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+
+    headers.each( function () {
+        var header = $(this);
+
+        if (!header.attr('id')) {
+            var sanitizedId = header.text().replace(/\s+/g, '_')
+                                           .replace(/[áàãâä]/gi, 'a')
+                                           .replace(/[éèêë]/gi, 'e')
+                                           .replace(/[íìîï]/gi, 'i')
+                                           .replace(/[óòõôö]/gi, 'o')
+                                           .replace(/[úùûü]/gi, 'u')
+                                           .replace(/[ç]/gi, 'c')
+                                           .replace(/[?!.,;:']/g, '');
+
+            header.attr('id', sanitizedId);
+        }
+
+        var link = $('<a/>')
+                        .text(header.text())
+                        .attr('class', 'toc')
+                        .attr('href', '#' + header.attr('id'));
+
+        tocList.append(link);
+    });
+
+    if (window.location.pathname !== '/') {
+        tocList.on('click', 'a', function () {
+            var targetOffset = $(
+                $(this).attr('href')
+            ).offset().top;
+
+            $('html, body').animate({
+                scrollTop: targetOffset - 50
+            }, 800);
+        });
+    }
+
+    if (currentHash && window.location.pathname !== '/') {
+        $('html, body').animate({
+            scrollTop: $(currentHash).offset().top - 50
+        }, 800);
+    }
+
+});
