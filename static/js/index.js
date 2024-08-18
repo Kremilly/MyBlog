7b5f91ee794f8d632d28bed6a9d9f44eed34b645
Kremilly <contact@kremilly.com>
2024-08-18 20:07:47 -0300

    refactor: improve search functionality in posts list

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,24 @@ $( _ => {
 
     let hideMenuTimeout;
 
+    setTimeout( function () {
+        let searchParam = new URL(window.location).searchParams.get('search');
+
+        if (searchParam) {
+            const inputElement = $('#postsSearch');
+
+            inputElement.val(searchParam);
+
+            inputElement.each( function () {
+                const event = new Event('input', {
+                    bubbles: true
+                });
+                
+                this.dispatchEvent(event);
+            });
+        }
+    }, 10);
+
     $(window).on('scroll', ScrollTo.checkScroll);
     $('#scrollToTopBtn').on('click', ScrollTo.top);
 
@@ -56,14 +74,30 @@ $( _ => {
 
     $('#postsSearch').on('input', function () {
         let searchText = $(this).val().toLowerCase();
+        history.replaceState(null, '', `?search=${searchText}`);
+
+        if (!searchText) {
+            let url = new URL(window.location);
+            url.searchParams.delete('search');
+            history.replaceState(null, '', url.pathname + url.search);
+        }
 
         $('.posts > a > .article').each( function () {
+            $('.posts > .no-results').remove();
             let listItemText = $(this).text().toLowerCase();
 
             if (listItemText.includes(searchText)) {
                 $(this).show();
             } else {
                 $(this).hide();
+                
+                if ($('.posts > a > .article:visible').length == 0) {
+                    $('.posts > .no-results').remove();
+                    
+                    $('.posts').append(`
+                        <div class='no-results'>Nenhuma postagem foi encontrada...</div>
+                    `);
+                }
             }
         });
     });
