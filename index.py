3220d0d0a921514cd8fe603449d87728642581dd
Kremilly <contact@kremilly.com>
2024-10-07 07:52:49 -0300

    refactor: fix injection code for fixes some errors

--- index.py
+++ index.py
@@ -2,13 +2,11 @@ from flask import Flask
 
 from routes.web import web
 from routes.api import api
-from routes.inject import inject
 
 app = Flask(__name__)
 
 app.register_blueprint(web)
 app.register_blueprint(api)
-app.register_blueprint(inject)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- routes/web.py
+++ routes/web.py
@@ -15,6 +15,30 @@ from backend.actions.export import Export
 
 web = Blueprint('web', __name__)
 
+@web.context_processor
+def inject_route_name():
+    return MyBlog.get_current_route()
+
+@web.context_processor
+def inject_route_post():
+    return MyBlog.check_if_post()
+
+@web.context_processor
+def inject_route_doc():
+    return MyBlog.check_if_docs()
+
+@web.context_processor
+def inject_route_doc_post():
+    return MyBlog.check_if_doc_post()
+
+@web.context_processor
+def inject_route_links_post():
+    return MyBlog.check_if_links()
+
+@web.context_processor
+def inject_route_home():
+    return MyBlog.check_if_home()
+
 @web.errorhandler(404)
 def page_not_found(e):
     return render_template(
@@ -30,11 +54,10 @@ def home():
         'index.html', 
         **MyBlog.common_template_args(),
         
-        title='Home',
         list_docs=Docs.list_docs(),
         list_posts=Posts.list_posts()
     ), 200
-
+    
 @web.route('/docs')
 def docs():
     return render_template(
@@ -42,11 +65,12 @@ def docs():
         **MyBlog.common_template_args(),
         
         title='Docs Hub',
+        
         list_docs=Docs.list_docs(),
         list_posts=Posts.list_posts(),
         list_categories=Docs.list_categories(),
     ), 200
-
+    
 @web.route('/links')
 def links():
     return render_template(
@@ -54,50 +78,53 @@ def links():
         **MyBlog.common_template_args(),
         
         title='Links',
+        
         list_links=Links.list_links(),
         social_media=Links.social_media(),
     ), 200
 
 @web.route('/blog/<post>')
-def post(post: str):
+def post(post:str):
     if Posts.check_post_exists(post):
         return render_template(
             'post.html', 
             **MyBlog.common_template_args(),
             
-            url=MyBlog.get_url(),
             html_content=Posts.get_post(post),
+            source_code=Posts.get_source_post(post),
+            
+            url=MyBlog.get_url(),
             qrcode=PostsMeta.get(post, 'QrCode'),
             title=PostsMeta.get_head_title(post),
-            source_code=Posts.get_source_post(post),
+            
+            post_cover=PostCover.generate(post),
+            post_export=PostsMeta.get(post, 'DownloadPdf'),
+            post_description=PostsMeta.get_description(post),
             
             post_links=Posts.list_links(post),
             post_date=PostsMeta.get_date(post),
-            post_cover=PostCover.generate(post),
             post_tags=PostsMeta.get_lists(post, 'tags'),
             post_read_time=PostsMeta.get_read_time(post),
             post_files=PostsMeta.get_lists(post, 'files'),
-            post_export=PostsMeta.get(post, 'DownloadPdf'),
-            post_description=PostsMeta.get_description(post),
             post_posts_recommends=Posts.list_posts_recommends(post),
         ), 200
-        
+    
     return redirect('/404'), 302
 
 @web.route('/blog/<item>/export')
-def export_item(item: str):
+def export_item(item:str):
     return Export.run(item)
 
 @web.route('/blog/<item>/raw')
-def source_item(item: str):
+def source_item(item:str):
     return Posts.get_source_post(item)
 
 @web.route('/docs/<api>/raw')
-def source_doc(api: str):
+def source_doc(api:str):
     return Docs.get_source_doc(api)
 
 @web.route('/docs/<api>')
-def doc(api: str):
+def doc(api:str):
     if Docs.check_doc_exists(api):
         return render_template(
             'post.html', 
@@ -105,15 +132,16 @@ def doc(api: str):
             
             url=MyBlog.get_url(),
             html_content=Docs.get_doc(api),
+            package=DocsMeta.get(api, 'Package'),
+            
             title=DocsMeta.get(api, 'Title'),
             qrcode=DocsMeta.get(api, 'QrCode'),
-            package=DocsMeta.get(api, 'Package'),
         ), 200
-        
+    
     return redirect('/404'), 302
 
 @web.route('/docs/<api>/export')
-def export_doc(api: str):
+def export_doc(api:str):
     return Export.run(api, True)
 
 @web.route('/rss')
